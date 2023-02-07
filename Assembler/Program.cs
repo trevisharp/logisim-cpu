9b53accc721bd2930dd9309679f27424bf0a15d8
Trevisan <38115923+trevisharp@users.noreply.github.com>
2023-02-07 17:00:07 -0300

    Update Program.cs

--- Assembler/Program.cs
+++ Assembler/Program.cs
@@ -1,5 +1,10 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+
+int pc = 0;
+var labels = new Dictionary<string, int>();
 
 if (args.Length == 0)
 {
@@ -44,12 +49,108 @@ finally
 
 string processLine(string line)
 {
-    byte[] opCode = new byte[16];
+    if (line.Contains(":"))
+    {
+        labels.Add(line.Trim().Replace(":", ""), pc);
+        return "";
+    }
+    pc++;
 
-    return toHex(opCode);
-}
+    var lnargs = line.Trim().Split(" ", 
+        StringSplitOptions.RemoveEmptyEntries);
 
-string toHex(byte[] code)
-{
-    return "0000";
+    switch (lnargs[0])
+    {
+        default:
+        case "nop":
+            return "0000";
+        
+        case "add":
+            return $"10{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "sub":
+            return $"11{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "mul":
+            return $"12{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "div":
+            return $"13{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "shl":
+            return $"14{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "shr":
+            return $"15{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "neg":
+            return $"16{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "and":
+            return $"17{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "or":
+            return $"18{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "not":
+            return $"19{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "xor":
+            return $"1a{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "inc":
+            return $"1b{hex(lnargs[1])}0";
+        
+        case "dec":
+            return $"1c{hex(lnargs[1])}0";
+        
+        case "push":
+            return $"23{hex(lnargs[1])}0";
+        
+        case "pop":
+            return $"24{hex(lnargs[1])}0";
+
+        case "cmp":
+            if (lnargs[1].Contains("$") && lnargs[2].Contains("$"))
+                return $"40{hex(lnargs[1])}{hex(lnargs[2])}";
+            else return $"5{hex(lnargs[1])}{hex(lnargs[2])}";
+        
+        case "mov":
+            if (lnargs[1].Contains("$") && lnargs[2].Contains("["))
+                return $"21{hex(lnargs[1])}{hex(lnargs[2])}";
+            else if (lnargs[1].Contains("[") && lnargs[2].Contains("$"))
+                return $"22{hex(lnargs[1])}{hex(lnargs[2])}";
+            else if (lnargs[1].Contains("$") && lnargs[2].Contains("$"))
+                return $"20{hex(lnargs[1])}{hex(lnargs[2])}";
+            else return $"2{hex(lnargs[1])}{hex(lnargs[2])}";
+            
+        case "call":
+            return $"e{hex(lnargs[1])}";
+        
+        case "ret":
+            return $"ffff";
+    }
+
+    string hex(string str)
+    {
+        str = str
+            .Replace("$", "")
+            .Replace(",", "")
+            .Replace("[", "")
+            .Replace("]", "");
+        var value = int.Parse(str);
+        var r = "";
+        while (value > 0)
+        {
+            r += inttohex(value % 16);
+            value /= 16;
+        }
+        if (r == "")
+            r = "0";
+        return r;
+    }
+
+    string inttohex(int value)
+        => value < 10 ? value.ToString() :
+        ((char)(value - 10 + 'a')).ToString();
 }
